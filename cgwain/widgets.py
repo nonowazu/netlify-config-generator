ef1f1bb2ebe083a9acdc9a3ab4fedfd9cca99b65
Oowazu Nonowazu <oowazu.nonowazu@gmail.com>
2022-10-07 00:10:26 -0500

    more widgets!

--- cgwain/widgets.py
+++ cgwain/widgets.py
@@ -41,8 +41,32 @@ class MarkdownWidget(Widget):
 class ListWidget(Widget):
     widget: str = 'list'
     fields: Widgets
+    summary: str | None = None
 
 class SelectWidget(Widget):
     widget: str = 'select'
     options: list[str]
     default: list[str]
+
+class ObjectWidget(Widget):
+    widget: str = 'object'
+    summary: str | None = None
+    collapsed: bool | None = None
+    fields: Widgets
+
+class ImageWidget(Widget):
+    widget: str = 'image'
+    choose_url: bool | None = None
+
+class RelationWidget(Widget):
+    widget: str = 'relation'
+    multiple: bool | None = None
+    collection: str
+    search_fields: list[str]
+    value_field: str
+    display_fields: list[str] | None = None
+
+class DateTimeWidget(Widget):
+    widget: str = 'datetime'
+    date_format: str | None = None
+    time_format: bool | None = None
